2963f5e923c2d94276d87da175a1a63aa49d9300
qtanran <1240429779@qq.com>
2023-05-22 22:39:44 +0800

    feat: 使用toJSON 和 trasnform 隐藏固定信息

--- app/model/user.js
+++ app/model/user.js
@@ -2,14 +2,25 @@ function initUserModel(app) {
   const mongoose = app.mongoose
   const Schema = mongoose.Schema
 
-  const UserSchema = new Schema({
-    username: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    nickName: { type: String },
-    picture: { type: String },
-    email: { type: String },
-    phoneNumber: { type: String }
-  }, { timestamps: true })
+  const UserSchema = new Schema(
+    {
+      username: { type: String, unique: true, required: true },
+      password: { type: String, required: true },
+      nickName: { type: String },
+      picture: { type: String },
+      email: { type: String },
+      phoneNumber: { type: String }
+    },
+    {
+      timestamps: true,
+      toJSON: {
+        transform(_doc, ret) {
+          delete ret.password
+          delete ret.__v
+        }
+      }
+    }
+  )
   return mongoose.model('User', UserSchema, 'user')
 }
 
